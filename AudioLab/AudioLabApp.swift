660d5a589f0035d8f5d215b71f4541b178a6422d
Yannis Lang <yannis.lang@hotmail.com>
2025-05-09 21:39:19 +0200

    Initial Commit

---
+++ AudioLab/AudioLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioLabApp.swift
+//  AudioLab
+//
+//  Created by Yannis Lang on 09/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AudioLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
